daaf44033fed24c4397b3d2098277e2020f71869
Tavicrack <gustavolizama841@gmail.com>
2025-12-08 15:35:56 -0600

    Se hizo un backup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage"; // <--- Importante
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.PUBLIC_API_KEY,
-  authDomain: import.meta.env.PUBLIC_AUTH_DOMAIN,
-  projectId: import.meta.env.PUBLIC_PROJECT_ID,
-  storageBucket: import.meta.env.PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.PUBLIC_MESSAGING_SENDER_ID,
-  appId: import.meta.env.PUBLIC_APP_ID
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app); // <--- Importante
+export const db = getFirestore(app);
